e25b066a896c758d0bdd0897cb72169800632b2f
John Brix Pomoy <johnbrixpomoy@192.168.88.254>
2025-04-17 03:26:42 -0700

    templated

---
+++ src/main/java/com/reactive/programming/controllers/ReactiveControllers.java
@@ -0,0 +1,13 @@
+package com.reactive.programming.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * package com.reactive.programming.controllers; /**
+ *
+ * @author <John Brix Pomoy>
+ * @version $Id: ReactiveControllers.java, v 0.1 2025-04-17 3:25 AM John Brix Pomoy Exp $$
+ */
+@RestController
+public class ReactiveControllers {
+}
